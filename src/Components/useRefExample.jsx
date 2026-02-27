c89e2a634e5bcfca29491c40e51796551f90bf7f
Goose03 <gusgt03@gmail.com>
2026-02-27 17:22:13 -0600

    useRef

--- src/Components/useRefExample.jsx
+++ src/Components/useRefExample.jsx
@@ -6,13 +6,23 @@ export default function useRefExample() {
 
   function handleClick() {
     ref.current = ref.current + 1;
+    console.log("Click");
+  }
+
+  function alertClick(){
     alert('You clicked ' + ref.current + ' times!');
   }
+
+  console.log("Render")
 
   return (
     <>
         <button onClick={handleClick}>
         Click me!
+        </button>
+
+        <button onClick={alertClick}>
+        Alert me!
         </button>
     </>
   );
